c57f31494837369de1335f3cb82b6a295bb9cd46
thephez <thephez@users.noreply.github.com>
2022-03-15 17:11:20 -0400

    feat: add identity tutorials

---
+++ 1. Identities and Names/identity-retrieve-account-ids.js
@@ -0,0 +1,23 @@
+const Dash = require('dash');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Dash.Client({
+  network: 'testnet',
+  wallet: {
+    mnemonic: process.env.MNEMONIC, // A Dash wallet mnemonic with testnet funds
+    unsafeOptions: {
+      skipSynchronizationBeforeHeight: 650000, // only sync from early-2022
+    },
+  },
+});
+
+const retrieveIdentityIds = async () => {
+  const account = await client.getWalletAccount();
+  return account.identities.getIdentityIds();
+};
+
+retrieveIdentityIds()
+  .then((d) => console.log('Mnemonic identities:\n', d))
+  .catch((e) => console.error('Something went wrong:\n', e))
+  .finally(() => client.disconnect());
---
+++ 1. Identities and Names/identity-retrieve.js
@@ -0,0 +1,14 @@
+const Dash = require('dash');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const client = new Dash.Client();
+
+const retrieveIdentity = async () => {
+  return client.platform.identities.get(process.env.IDENTITY_ID); // Your identity ID
+};
+
+retrieveIdentity()
+  .then((d) => console.log('Identity retrieved:\n', d.toJSON()))
+  .catch((e) => console.error('Something went wrong:\n', e))
+  .finally(() => client.disconnect());
