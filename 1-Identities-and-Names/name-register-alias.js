71b95ad2ba110c24613bc9d49bfa79b88ea317fd
thephez <thephez@users.noreply.github.com>
2022-03-16 09:24:03 -0400

    feat: names

---
+++ 1-Identities-and-Names/name-register-alias.js
@@ -0,0 +1,32 @@
+const Dash = require('dash');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const aliasToRegister = 'abc-alias'; // Enter alias to register
+
+const clientOpts = {
+  wallet: {
+    mnemonic: process.env.MNEMONIC,  // A Dash wallet mnemonic with testnet funds
+    unsafeOptions: {
+      skipSynchronizationBeforeHeight: 650000, // only sync from early-2022
+    },
+  },
+};
+const client = new Dash.Client(clientOpts);
+
+const registerAlias = async () => {
+  const platform = client.platform;
+  const identity = await platform.identities.get(process.env.IDENTITY_ID); // Your identity ID
+  const aliasRegistration = await platform.names.register(
+    `${aliasToRegister}.dash`,
+    { dashAliasIdentityId: identity.getId() },
+    identity,
+  );
+
+  return aliasRegistration;
+};
+
+registerAlias()
+  .then((d) => console.log('Alias registered:\n', d.toJSON()))
+  .catch((e) => console.error('Something went wrong:\n', e))
+  .finally(() => client.disconnect());
